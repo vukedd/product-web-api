93992ed6e8498f97fffd09357a7c9439b42b8a2b
Vukasin <137070008+vukedd@users.noreply.github.com>
2024-09-16 13:03:23 +0200

    Delete PresentationLayer directory

--- PresentationLayer/DTO/NewUserDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProductWebAPI.DTO
-{
-    public class NewUserDTO
-    {
-        public string UserName { get; set; }
-        public string Email { get; set; }
-        public string Token { get; set; }
-    }
-}
